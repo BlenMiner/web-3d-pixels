d2879bd3ad48166f9814eaf635dfc9c4fdcac945
Valentin Bordeianu <blenminer123@gmail.com>
2023-11-18 13:21:13 +0100

    changed starting template

--- 3d-pixels-server/Program.cs
+++ 3d-pixels-server/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PixelsServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
